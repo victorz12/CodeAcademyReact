10f69c28c4172224ff42f037e5253db994e9c22c
victorz12 <43283981+victorz12@users.noreply.github.com>
2019-01-17 16:01:44 -0600

    Add files via upload

---
+++ PROJETC8.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myDiv = <div className="big">I AM A BIG DIV</div>;
+
+ReactDOM.render(myDiv, document.getElementById('app'));
+
+
+
+
+/*  ejercico  9 */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1>2 + 3</h1>,
+  document.getElementById('app')
+);
+
+
+/* ejercicio 10 */
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1>{2 + 3}</h1>,
+  document.getElementById('app')
+);
